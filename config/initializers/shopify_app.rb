e89d2affec5c34574ea239da03a576bf033db5b8
Christopher J. Mader <christopherjmader@gmail.com>
2017-09-09 20:19:56 -0500

    added secret

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
   config.api_key = "917e4819c12c3d2ec9901101fa5201d8"
-  config.secret = "<secret>"
+  config.secret = "3177e470be415febf52382eeea0e2153"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
